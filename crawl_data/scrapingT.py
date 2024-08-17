e287ff5e4ddd6b5c6d554e4bf0ef067bcf17978d
Cong Nguyen <nkcong206@gmail.com>
2024-08-18 00:03:47 +0700

    Co-authored-by: .NmHunG__ <mhungdangbuonngu@users.noreply.github.com> Co-authored-by: Nguyễn Minh Khôi <ShadowFiendz@users.noreply.github.com>

--- crawl_data/scrapingT.py
+++ crawl_data/scrapingT.py
@@ -1,66 +1,50 @@
 import requests
 from bs4 import BeautifulSoup as bp
 import csv
-from datetime import datetime
+import psycopg2
 
-USER_AGENT='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
 REQUEST_HEADER = {
     'User-Agent': USER_AGENT,
     'Accept-language': 'en-US, en;q=0.5',
 }
+
 def get_page_html(url):
-    res= requests.get(url=url,headers= REQUEST_HEADER)
-    return res.content
+    res = requests.get(url=url, headers=REQUEST_HEADER)
+    return res.text
 
-
-def get_hotel_price(soup):
-    price_element=soup.find('div', class_ = 'css-901oao r-a5wbuh r-b88u0q r-135wba7 r-1ff274t')
-    if price_element:
-        true_price = price_element.text.strip().replace('VND', '').replace('.', '')
-        try:
-            return float(true_price)
-        except ValueError:
-            print('value obtained for price cannot find')
-            exit()
-    return None
 def get_hotel_name(soup):
-    name=soup.find('h1',class_='css-4rbku5 css-901oao css-cens5h r-cwxd7f r-a5wbuh r-1x35g6 r-b88u0q r-fdjqy7')
+    name = soup.find('div', class_='css-901oao r-a5wbuh r-1enofrn r-b88u0q r-1cwl3u0 r-fdjqy7 r-3s2u2q')
     return name.text.strip() if name else None
 
-
-def get_hotel_rating(soup):
-    rating=soup.find('div',class_ = 'css-901oao r-a5wbuh r-1x35g6 r-b88u0q r-fdjqy7')
-    return rating.text.strip() if rating else None
-
-
-def get_hotel_des(soup):
-    des=soup.find('div',attrs={'style':'font-family:Godwit, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;font-size:14px;line-height:20px;max-height:80px;overflow:hidden'})
-    return des.text.strip() if des else None
-
-def get_hotel_location(soup):
-    location=soup.find('div',class_='css-901oao css-cens5h r-cwxd7f r-13awgt0 r-a5wbuh r-1b43r93 r-majxgm r-rjixqe r-fdjqy7')
-    return location.text.strip() if location else None
-
-def extract_hotels_url(url):
-    info={}
-    print(f"scraping URL number: {1}")
+def extract_hotels_url(url, i):
+    info = {}
+    print(f"Scraping URL number: {i}")
     html = get_page_html(url=url)
-    soup=bp(html,'lxml')
+    soup = bp(html, 'lxml')
     info['name'] = get_hotel_name(soup)
-    info['price']=get_hotel_price(soup)
-    info['rating']=get_hotel_rating(soup)
-    info['location'] = get_hotel_location(soup)
-    info['description']=get_hotel_des(soup)
     return info
 
-if __name__ =="__main__":
-    data=[]
-    with open('hotels.csv',newline='') as csvfile:
+if __name__ == "__main__":
+    conn = psycopg2.connect(
+        database="test", 
+        user="postgres", 
+        host='localhost',
+        password="cong2006",
+        port=5432
+    )
+    cur = conn.cursor()
+    
+    with open('hotels.csv', newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        for row in reader:
-            url=row[0]
-            data.append(extract_hotels_url(url))
-    
-    
-    
-            
+        
+        for i, row in enumerate(reader):
+            url = row[0]
+            data = extract_hotels_url(url, i)
+            sql_query = """INSERT INTO Hotel (Name) VALUES (%s)"""
+            cur.execute(sql_query, (data['name'],))  # Note the comma after data['name']
+            conn.commit()
+            print(f'Data for {i} inserted into DB')
+
+    cur.close()
+    conn.close()
